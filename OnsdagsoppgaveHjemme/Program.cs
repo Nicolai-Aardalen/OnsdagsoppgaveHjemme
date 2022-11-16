1b18d0d4bac27983dd22435e4e961b3171bbdfa1
Nicolai-Aardalen <Nicolaiaar@viken.no>
2022-11-16 15:35:11 +0100

    Add project files.

---
+++ OnsdagsoppgaveHjemme/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnsdagsoppgaveHjemme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            antallTallIEnInt();
+        }
+        static void antallTallIEnInt()
+        {
+            Console.Write("Write a multi-digit number: ");
+            bool x = int.TryParse(Console.ReadLine(), out int number);
+            int count = 0;
+            if (!x)
+            {
+                return;
+            }
+            while (number > 0)
+            {
+                number = number / 10;
+                count++;
+            }
+            Console.WriteLine("\nThe number of numbers in this number is: " + count);
+            Console.ReadLine();
+        }
+    }
+}
